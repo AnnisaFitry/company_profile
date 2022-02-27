5e7298c922f43069668663d44226135e9debfbd2
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-02-27 17:54:15 +0700

    hasil_tugas_1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageCont;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,8 +11,48 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/about_us', function () {
+    return view('about');
+});
+Route::prefix('product')->group(function(){
+    Route::get('/1', function () {
+        return view('pd_edu');
+    });
+    Route::get('/2', function () {
+        return view('pd_kids');
+    });
+    Route::get('/3', function () {
+        return view('pd_story');
+    });
+    Route::get('/4', function () {
+        return view('pd_song');
+    });
+});
+Route::prefix('program')->group(function(){
+    Route::get('/1', function () {
+        return view('pro_karir');
+    });
+    Route::get('/2', function () {
+        return view('pro_magang');
+    });
+    Route::get('/3', function () {
+        return view('pro_kunjndus');
+    });
+});
+Route::get('/news/{id?}', function ($id = 1) {
+    return view('new');
+});
+Route::get('/news/{id?}', function ($id = 2) {
+    return view('newedu');
+});
+Route::get('/ContactUs', [PageCont::class, 'ContactUs']);
